950527e38de6ea2bca2d7363dcebedcdd6d5c746
Semen Kuzmin <eurosemen@yandex.ru>
2022-05-09 20:06:28 +0300

    added tests

--- src/test/java/ru/netology/postman/PostmanEchoTest1.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest1.java
@@ -1,2 +1,43 @@
-package ru.netology.postman;public class PostmanEchoTest1 {
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest1 {
+    @Test
+    void shouldPostData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldPostCyrillic() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("некоторые данные") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("некоторые данные"))
+        ;
+    }
 }
